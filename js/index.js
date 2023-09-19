67e4981f632ec45c312e5574f07cd137eeb80d53
ngockim109 <ngockim9047@gmail.com>
2023-09-19 13:46:57 +0700

    add jquery to index.html

---
+++ js/index.js
@@ -0,0 +1,26 @@
+// $(function () {
+//   $(".home").on("click", alert("Home"));
+// });
+// $(function () {
+//   $(".aboutus").on("click", alert("About us"));
+// });
+
+// $(function () {
+//   $(".contact").on("click", alert("Contact us"));
+// });
+
+// $(function () {
+//   $(".menu").on("click", alert("Menu"));
+// });
+
+$(document).ready(function () {
+  $(".home").click(function () {
+    alert("Home");
+  });
+  $(".aboutus").click(function () {
+    alert("About us");
+  });
+  $(".menu").click(function () {
+    alert("Menu");
+  });
+});
